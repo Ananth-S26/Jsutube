bc27343e75083ab248b3f6ba20a4d7ff04a86dc8
Ananth-S26 <ananthsivakumar26@gmail.com>
2025-06-25 14:45:48 +0530

    lesson10

---
+++ 10.htmlCssJs/script/10.js
@@ -0,0 +1,30 @@
+function handleCostKeyDown(event){
+    console.log(event.key);
+    if (event.key==='Enter'){
+        calculate();
+    }
+}
+        
+function calculate(){
+    const inputElement= document.querySelector('.js-cost-input');
+    let cost =Number(inputElement.value);
+    if(cost<40){
+        cost=cost+10;
+    }
+        document.querySelector('.jsTotalCost').innerHTML=`$${cost.toFixed(2)}`;
+}
+
+
+
+function subscribe(){
+    const buttonElement = document.querySelector('.jsb');
+
+    if (buttonElement.innerText==='subscribe'){
+        buttonElement.innerText='subscribed';
+        buttonElement.classList.add('jsbd');
+    }
+    else{
+        buttonElement.innerText='subscribe';
+        buttonElement.classList.remove('jsbd');
+    }
+}
